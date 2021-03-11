325d46f1cfd71281f0198cdd67e8bf67d071919f
Eduan Bekker <eduan@truckin-it.com>
2021-03-11 19:36:37 +0200

    Added zipkin

--- account/src/main/kotlin/com/eduanbekker/account/AccountConfiguration.kt
+++ account/src/main/kotlin/com/eduanbekker/account/AccountConfiguration.kt
@@ -1,27 +1,31 @@
 package com.eduanbekker.account
 
 import com.eduanbekker.account.domain.AccountRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.messaging.Message
 import java.util.function.Function
 
 @Configuration
 class AccountConfiguration {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
     /**
      * Note this implementation intentionally uses a lot of Kotlin features for demonstration purposes.
      * These include extension methods, scoped functions, elvis operator etc
      * This is in contrast to the implementation in InventoryConfiguration
      */
     @Bean
-    fun updateAccount(accountRepository: AccountRepository) = Function<AccountChangeRequest, Any> { request ->
-        accountRepository.findByIdOrNull(request.id)
-            ?.also { println("Updating account: $it") }
-            ?.apply { balance = balance.change(request.type, request.amount) }
-            ?.let { println("Account is now: $it"); accountRepository.save(it) }
+    fun updateAccount(accountRepository: AccountRepository) = Function<Message<AccountChangeRequest>, Any> { request ->
+        accountRepository.findByIdOrNull(request.payload.id)
+            ?.also { logger.info("Updating account: $it") }
+            ?.apply { balance = balance.change(request.payload.type, request.payload.amount) }
+            ?.let { logger.info("Account is now: $it"); accountRepository.save(it) }
 
-            ?: "Could not find account: ${request.id}"
-                .also { println(it) }
+            ?: "Could not find account: ${request.payload.id}"
+                .also { logger.info(it) }
     }
 }
 
--- sales-gateway/src/main/kotlin/com/eduanbekker/salesgateway/SalesController.kt
+++ sales-gateway/src/main/kotlin/com/eduanbekker/salesgateway/SalesController.kt
@@ -1,6 +1,9 @@
 package com.eduanbekker.salesgateway
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.function.StreamBridge
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController
 class SalesController(val inventoryClient: InventoryClient,
                       val streamBridge: StreamBridge) {
 
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
     @PostMapping("/buy")
     fun buy(@RequestParam account: Long, @RequestParam item: Long) {
+        logger.info("Request to buy item: $item for account $account")
         val inventoryItem = inventoryClient.getItem(item)
-        streamBridge.send("updateAccount-in-0", AccountChangeRequest(account, inventoryItem.price, ChangeType.SUBTRACT))
-        streamBridge.send("updateInventory-in-0", InventoryUpdateRequest(account, 1, ChangeType.SUBTRACT))
+        streamBridge.send("updateAccount-in-0",
+            MessageBuilder.withPayload(AccountChangeRequest(account, inventoryItem.price, ChangeType.SUBTRACT)).build())
+        streamBridge.send("updateInventory-in-0",
+            MessageBuilder.withPayload(InventoryUpdateRequest(account, 1, ChangeType.SUBTRACT)).build())
     }
 
 }
