298a862f586da68d1e4d2bcfa503caed00c721e6
Eduan Bekker <eduan@truckin-it.com>
2021-03-12 09:30:48 +0200

    Cleanup of meta

--- sales-meta/src/main/kotlin/com/eduanbekker/salesmeta/SalesMeta.kt
+++ sales-meta/src/main/kotlin/com/eduanbekker/salesmeta/SalesMeta.kt
@@ -2,10 +2,8 @@ package com.eduanbekker.salesmeta
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
-@EnableWebMvc
 class SalesMeta
 
 fun main(args: Array<String>) {
